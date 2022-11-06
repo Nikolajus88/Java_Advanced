f4699035faa71cabf866bff9c2980c54308ea9af
nikol <nikolajus.metlickis@gmail.com>
2022-11-06 11:13:49 +0000

    new commit

--- src/main/java/day12_0611/intro_data_structures/DataStructuresMain.java
+++ src/main/java/day12_0611/intro_data_structures/DataStructuresMain.java
@@ -1,7 +1,10 @@
 package day12_0611.intro_data_structures;
 
+import java.sql.SQLOutput;
+
 public class DataStructuresMain {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("world");
     }
 }
